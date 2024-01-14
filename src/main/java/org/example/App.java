c482ee579331ecba39f68f89ce857b2994294bda
MiaoGL <645517131@qq.com>
2024-01-14 15:29:37 +0800

    dev third commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,5 +11,7 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!q" );
         System.out.println( "222" );
+
+        System.out.println("dev-3");
     }
 }
